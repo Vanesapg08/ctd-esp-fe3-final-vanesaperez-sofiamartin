3a9608d2975c7df74e2f7892818b1693b563b09f
Vanesa Pérez Guerra <106204516+Vanesapg08@users.noreply.github.com>
2023-09-12 02:03:04 +0000

    Anadi el Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Navbar.css';
 
-//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
-
-const Navbar = () => {
+function Navbar() {
+  const [darkMode, setDarkMode] = useState(false); // Por defecto es el tema claro
 
   return (
-    <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
-      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
-    </nav>
-  )
+    <div className={`header ${darkMode ? 'dark' : ''}`}>
+      <span className="logo">
+        <span className="logo-red">D</span>H Odonto
+      </span>
+
+      <div className="nav-links">
+        <a href="#home">Home</a>
+        <a href="#contact">Contact</a>
+        <a href="#favs">Favs</a>
+      </div>
+
+      <button onClick={() => setDarkMode(!darkMode)}>
+        {darkMode ? '🌞' : '🌙'}
+      </button>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
