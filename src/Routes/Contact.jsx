7769e1b8e800205c785fc7c6003f613a78158a4b
Sofia Martin <106210554+SofiMartin@users.noreply.github.com>
2023-09-15 02:06:23 +0000

    acomodando contact

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import Form from '../Components/Form'
+import { ContextGlobal } from "../Components/utils/global.context";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
+
 const Contact = () => {
+  const { state } = useContext(ContextGlobal)
+
   return (
-    <div>
-      <h2>Want to know more?</h2>
-      <p>Send us your questions and we will contact you</p>
+    <main className={state.theme}>
+      <h1>¿Quieres más información?</h1>
+      <p style={{textAlign:"center"}}>Envia todas tus dudas y te contactaremos pronto</p>
       <Form/>
-    </div>
+    </main>
   )
 }
 
